57b48044ff35cce8f8912a1b65b83f989716f30c
Whitni <whitnikdavis@gmail.com>
2020-04-02 17:05:41 -0500

    Updates

---
+++ precache-manifest.c7738e3dd40a2baa5d8ec01cab4ff416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87321b6b4401b9597021d1bb37219d02",
+    "url": "/token-editor/index.html"
+  },
+  {
+    "revision": "34e72feca054abe23fb2",
+    "url": "/token-editor/static/css/2.14921843.chunk.css"
+  },
+  {
+    "revision": "3a1220d19538e1845737",
+    "url": "/token-editor/static/css/main.144c8b47.chunk.css"
+  },
+  {
+    "revision": "34e72feca054abe23fb2",
+    "url": "/token-editor/static/js/2.42dfe7d6.chunk.js"
+  },
+  {
+    "revision": "34857d5e17ebeec03a3c7521cf933911",
+    "url": "/token-editor/static/js/2.42dfe7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1220d19538e1845737",
+    "url": "/token-editor/static/js/main.0754a197.chunk.js"
+  },
+  {
+    "revision": "ef4264ecfcd2e749b1f3",
+    "url": "/token-editor/static/js/runtime-main.f9256278.js"
+  }
+]);
