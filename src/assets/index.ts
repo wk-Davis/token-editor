c2b7dabf803e36f7f3db31ce322fe28857ee6657
Whitni <whitnikdavis@gmail.com>
2020-04-05 17:40:55 -0500

    Rearrange assets and add icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,8 @@
-import cleric from './cleric/cleric_lineart.png';
-import gunslinger from './gunslinger/gunslinger_lineart.png';
-import inquisitor from './inquisitor/inquisitor_lineart.png';
-import monk from './monk/monk_lineart.png';
-import ranger from './ranger/ranger_lineart.png';
+import cleric from './tokens/cleric/cleric_lineart.png';
+import gunslinger from './tokens/gunslinger/gunslinger_lineart.png';
+import inquisitor from './tokens/inquisitor/inquisitor_lineart.png';
+import monk from './tokens/monk/monk_lineart.png';
+import ranger from './tokens/ranger/ranger_lineart.png';
 
 interface srcRef {
   src: string;
